fcf88c5ce33666e9ca5cefb2ba2dbf01adf9afbd
Asembo <64367365+Asembo@users.noreply.github.com>
2021-10-07 12:44:51 -0500

    Update SocketClient.java

--- SocketClient.java
+++ SocketClient.java
@@ -13,27 +13,25 @@ public class Client {
     int listeningPort;
     Socket clientConnection;
     PrintWriter output;
-    BufferReader input;
-    InputStreamReader data;
+    Message message;
 
     //constructor
-	public Client()
+	public void socketClient()
 	{
 		//Your code goes here
 		//establish dedicated connection to each neighbor using TCP/IP sockets
 
         NodeID neighbors = getNeighbors(); //run function that returns array of current Node's neighbors
 		for(i = 0; i < neighbors.length; i++){ //for each neighbor establish connection
+            
 			//retain the same socket till program terminates	
 			while (true) {
 				try{
 					Socket clientConnection = new Socket("hostname", listeningPort); //for each neighbor, establish a connection 
-					System.out.println("Connected"); 
-                    Printwriter output = new PrintWriter(clientConnection.getOutputStream(), true);
-                    InputStreamReader data = new InputStreamReader(clientConnection.getInputStream());
-                    BufferReader input = BufferReader(message); 
-                    PrintWriter output.println (message);  
-                    Message message =  (NodeID myID, input.readLine();
+
+                    Printwriter output = new PrintWriter(clientConnection.getOutputStream(), true); 
+                    byte [] data = output.println (message);  
+                    Message message =  (NodeID myID, byte [] data);
 				}
 				catch(UnknownHostException ex){
 					//mostly nothing to do here
